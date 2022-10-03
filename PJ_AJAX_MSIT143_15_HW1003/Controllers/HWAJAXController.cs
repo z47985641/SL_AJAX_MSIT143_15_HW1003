d43f3d6c4228878568b118af804c63e0a4f6e905
z47985641 <z47985641@gmail.com>
2022-10-04 01:01:06 +0800

    加入專案檔案。

---
+++ PJ_AJAX_MSIT143_15_HW1003/Controllers/HWAJAXController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PJ_AJAX_MSIT143_15_HW1003.Controllers
+{
+    public class HWAJAXController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PJ_AJAX_MSIT143_15_HW1003/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PJ_AJAX_MSIT143_15_HW1003.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
